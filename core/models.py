8215ceb4d37e0a1abac104ddd41cb38e2864b0b2
Alexey <vaznoe2024@mail.ru>
2025-09-25 01:04:32 +0300

    v.0 Созданна и авторизированна модель сниппета

--- core/models.py
+++ core/models.py
@@ -1,3 +1,36 @@
 from django.db import models
 
-# Create your models here.
+LANGUAGE_CHOICES = [
+    ('python', 'Python'),
+    ('javascript', 'JavaScript'),
+    ('java', 'Java'),
+    ('html', 'HTML'),
+]
+
+STYLE_CHOICES = [
+    ('friendly', 'Friendly'),
+    ('monokai', 'Monokai'),
+    ('solarized', 'Solarized'),
+]
+
+class Snippet(models.Model):
+    created = models.DateField(auto_now_add=True)
+    title = models.CharField(max_length=100, blank = True, default='')
+    code = models.TextField()
+    lineos = models.BooleanField(default=False)
+    language = models.CharField(
+        choices = LANGUAGE_CHOICES,
+        default = 'python',
+        max_length=100
+    )
+    style = models.CharField(
+        choices=STYLE_CHOICES, 
+        default='friendly', 
+        max_length=100
+    )
+    
+    class Meta:
+        ordering = ['created']
+        
+    def __str__(self):
+        return self.title or f"Snippet #{self.id}"
